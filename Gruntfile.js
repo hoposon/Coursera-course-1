1aeb7adc71fbeb8d1e7720f8e75ac35f380e044e
Lukas Houf <hoposon@gmail.com>
2019-04-12 08:20:24 +0200

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,16 @@
-// 'use strict'
-
-// const timeGrunt = require('time-grunt');
-// const sass = require('node-sass');
-
-// const watch = require('grunt-contrib-watch');
-// const browserSync = require('grunt-browser-sync');
-// const jitGrunt = require('jit-grunt');
+'use strict'
 
 module.exports = function(grunt) {
-	// timeGrunt(grunt);
-	// jitGrunt(grunt);
-	// watch(grunt);
-	
-	// sass(grunt);
-	// browserSync(grunt);
 
 	const sass = require('node-sass');
-	require('load-grunt-tasks')(grunt);
+	require('time-grunt')(grunt);
+	require('jit-grunt')(grunt, {
+		useminPrepare: 'grunt-usemin'
+	})
+	// const browserSync = require('grunt-browser-sync')(grunt);
+	// browserSync(grunt);
 
-	const browserSync = require('grunt-browser-sync');
-	browserSync(grunt);
+	// require('load-grunt-tasks')(grunt);
 
 	grunt.initConfig({
 		sass: {
@@ -55,14 +46,120 @@ module.exports = function(grunt) {
 					}
 				}
 			}
+		},
+		clean: {
+			build: {
+				src: ['dist/']
+			}
+		},
+		copy: {
+			main: {
+				files: [
+					{expand: true, src: ['./*.html'], dest: 'dist/'},
+					{expand: true, cwd: './node_modules/font-awesome/', src: ['fonts/*.*'],	dest: 'dist/'}
+				]
+			}
+		},
+		imagemin: {
+			png: {
+				options: {
+					progressive: true
+				},
+				files: [
+					{expand: true, cwd: 'img/', src: ['**/*.png'], dest: 'dist/img'}
+				]
+			}
+		},
+		useminPrepare: {
+			foo: {
+				dest: 'dist',
+				src: ['contactus.html', 'aboutus.html', 'index.html']
+			},
+			options: {
+				flow: {
+					steps: {
+						css: ['cssmin'],
+						js: ['uglify']
+					},
+					post: {
+						css: [{
+							name: 'cssmin',
+							createConfig: function(context, block) {
+								var generated = context.options.generated;
+								generated.options = {
+									keepSpecialComments: 0, rebase: false
+								};
+							}
+						}]
+					}
+				}
+			}
+		},
+		concat: {
+			options: {
+				separator: ';'
+			},
+			dist: {}
+		},
+		uglify: {
+			dist: {}
+		},
+		cssmin: {
+			dist: {}
+		},
+		filerev: {
+			options: {
+				encoding: 'utf8',
+				algorithm: 'md5',
+				length: 20
+			},
+			release: {
+				files: [{
+					src: [
+						'dist/js/*.js',
+						'dist/css/*.css'
+					]
+				}]
+			}
+		},
+		usemin: {
+			html: ['dist/contactus.html', 'dist/aboutus.html', 'dist/index.html'],
+			options: {
+				assetsDirs: ['dist', 'dist/css', 'dist/js']
+			}
+		},
+		htmlmin: {
+			dist: {
+				options: {
+					removeComments: true,
+        			collapseWhitespace: true
+				},
+				files: {
+					'dist/index.html': 'dist/index.html',
+					'dist/contactus.html': 'dist/contactus.html',
+					'dist/aboutus.html': 'dist/aboutus.html'
+				}
+			}
 		}
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-watch');
+	//replaced with the jit-grunt
+	// grunt.loadNpmTasks('grunt-contrib-sass');
+	// grunt.loadNpmTasks('grunt-contrib-watch');
+	// grunt.loadNpmTasks('grunt-browser-sync');
 
-	// grunt.registerTask('css', ['sass']);
+	// grunt.loadNpmTasks('grunt-contrib-clean');
+	// grunt.loadNpmTasks('grunt-contrib-copy');
+	// grunt.loadNpmTasks('grunt-contrib-imagemin');
+	// grunt.loadNpmTasks('grunt-contrib-usemin');
+	// grunt.loadNpmTasks('grunt-contrib-concat');
+	// grunt.loadNpmTasks('grunt-contrib-cssmin');
+	// grunt.loadNpmTasks('grunt-contrib-uglify');
+	// grunt.loadNpmTasks('grunt-filerev');
+	// grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
-	//grunt.registerTask('watch', ['watch']);
+
+	grunt.registerTask('build', ['clean','copy','imagemin', 'useminPrepare', 'concat', 'cssmin', 'uglify', 'filerev', 'usemin', 'htmlmin']);
 
 	grunt.registerTask('default', ['browserSync', 'watch']);
 }
